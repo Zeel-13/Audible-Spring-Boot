8030274e35c53ff70801e3c4a8027d1e4c9223fe
Zeel-13 <zeeldhengre265512@gmail.com>
2025-05-02 12:29:33 +0530

    validation handling added to audiobook MS

--- AudiobookService/src/main/java/com/audible/AudiobookService/exception/GlobalExceptionHandler.java
+++ AudiobookService/src/main/java/com/audible/AudiobookService/exception/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package com.audible.AudiobookService.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -53,6 +58,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	    System.out.println("RuntimeException handled globally in audiobook service");
 	    return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
+	
+	@Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> body = createErrorBody(HttpStatus.BAD_REQUEST, "Validation failed on request body", request);
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 
 }
 
